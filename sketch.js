23dd3ed46181976b677015fd313d415db18187e3
10832613 <85686887+10832613@users.noreply.github.com>
2021-06-12 14:52:47 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
+let imageModelURL = 'https://10832613.github.io/imageaii/';
 
 // Video
 let video;
